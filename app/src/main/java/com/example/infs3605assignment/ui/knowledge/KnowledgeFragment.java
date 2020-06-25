f4a37c5b5accdfab630ed9c7a4caa45a43151d2d
annnabel <55646347+annnabel@users.noreply.github.com>
2020-06-25 13:57:38 +1000

    - material theming - module recycler view

--- app/src/main/java/com/example/infs3605assignment/ui/knowledge/KnowledgeFragment.java
+++ app/src/main/java/com/example/infs3605assignment/ui/knowledge/KnowledgeFragment.java
@@ -11,17 +11,47 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.infs3605assignment.R;
 
+import java.util.ArrayList;
+
 public class KnowledgeFragment extends Fragment {
 
+    private RecyclerView recyclerView;
+    private androidx.recyclerview.widget.RecyclerView.LayoutManager layoutManager;
+    private RecyclerView.Adapter adapter;
 
-    /*public View onCreateView(@NonNull LayoutInflater inflater,
+    public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
 
-        View root = inflater.inflate(R.layout.fragment_home, container, false);
+        // Inflate view with recycler module fragment
+        View root = inflater.inflate(R.layout.fragment_module_recycler, container, false);
+
+        // Initialise recyclerView
+        recyclerView = root.findViewById(R.id.recyclerView);
+        recyclerView.setHasFixedSize(true);
+
+        // Initialise layoutManager for recyclerView
+        layoutManager = new LinearLayoutManager(getActivity());
+        recyclerView.setLayoutManager(layoutManager);
+
+        // Get categories from LearnCategories class
+        ArrayList<ModuleCategories> categories = ModuleCategories.getCategories();
+
+        // Create adapter object
+        adapter = new ModuleAdapter(this.getContext(), categories);
+
+        // Attach adapter to recycler
+        recyclerView.setAdapter(adapter);
 
         return root;
-    }*/
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
 }
